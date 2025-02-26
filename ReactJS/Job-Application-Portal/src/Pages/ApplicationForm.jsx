03e9c04ec829fd0b7b23ff11875b77675b69792f
Himanshu Gupta <101guptaji@gmail.com>
2025-02-26 23:03:45 +0530

    Used Custom useForm hook for form validation

--- ReactJS/Job-Application-Portal/src/Pages/ApplicationForm.jsx
+++ ReactJS/Job-Application-Portal/src/Pages/ApplicationForm.jsx
@@ -1,11 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { addApplication, editApplication } from '../Redux/Slice/applicationSlice'
 
 // Validation
-import * as Yup from 'yup';
+const validate = (values) => {
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = "Name is required";
+  }
+
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (!values.phone) {
+    errors.phone = "Phone No. is required";
+  }
+  else if (!/^[6-9]\d{9}$/.test(values.phone)) {
+    errors.phone = "Phone No. is invalid";
+  }
+
+  return errors;
+};
+
+import useForm from '../Hooks/useForm';
 
 const ApplicationForm = ({ jobs }) => {
   const navigate = useNavigate();
@@ -33,11 +56,6 @@ const ApplicationForm = ({ jobs }) => {
   const applications = useSelector(state => state.application.applications)
 
   const existingApplication = applications.find(app => app.jobID === id);
-  // console.log(existingApplication);
-
-  const [application, setApplication] = useState(typeof existingApplication === "undefined" ? initialState : existingApplication);
-  // console.log(application);
-
 
   const randId = crypto.randomUUID();
 
@@ -45,25 +63,10 @@ const ApplicationForm = ({ jobs }) => {
   const [step2, setStep2] = useState(false);
   const [step3, setStep3] = useState(false);
 
-  // validation using Yup
-  const validationSchema = Yup.object().shape({
-    name: Yup.string()
-      .required("Name is required"),
-    email: Yup.string()
-      .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email")
-      .required("Email is required"),
-    phone: Yup.string()
-      .matches(/^[6-9]\d{9}$/, "Invalid Phone No.")
-      .required("Phone No. is required"),
-  });
-
-  const [errors, setErrors] = useState({});
-
-  function handleChange(e) {
-    e.preventDefault();
-    const { name, value } = e.target;
-    setApplication({ ...application, [name]: value });
-  }
+  const { values, errors, handleChange, validateForm } = useForm(
+    typeof existingApplication === "undefined" ? initialState : existingApplication, // Initial values
+    validate // Validation function
+  );
 
   function handleBack(e) {
     e.preventDefault();
@@ -80,9 +83,7 @@ const ApplicationForm = ({ jobs }) => {
 
   async function handleNext(e) {
     e.preventDefault();
-    try {
-      await validationSchema.validate(application, { abortEarly: false });
-      setErrors({});
+    if (validateForm()) {
       if (step1) {
         setStep1(false);
         setStep2(true);
@@ -91,16 +92,7 @@ const ApplicationForm = ({ jobs }) => {
         setStep2(false);
         setStep3(true);
       }
-      console.log(application);
-    }
-    catch (error) {
-      const validationErrors = {};
-      if (error.inner) {
-        error.inner.forEach((err) => {
-          validationErrors[err.path] = err.message
-        })
-      }
-      setErrors(validationErrors);
+      console.log(values);
     }
   }
 
@@ -110,18 +102,19 @@ const ApplicationForm = ({ jobs }) => {
       setStep3(false);
       setStep1(true);
     }
-    console.log(application);
+    console.log(values);
 
-    if (application.id) {
-      dispatch(editApplication(application));
-      navigate(`/applications/${application.id}`);
+    if (values.id) {
+      dispatch(editApplication(values));
+      navigate(`/applications/${values.id}`);
     }
     else {
-      dispatch(addApplication({ ...application, id: randId }));
+      dispatch(addApplication({ ...values, id: randId }));
       navigate(`/applications/${randId}`);
     }
-
   }
+
+  const minDate = new Date();
 
   return (
 
@@ -131,18 +124,18 @@ const ApplicationForm = ({ jobs }) => {
         <form className='form'>
           <div>
             <label>Name:</label>
-            <input type='text' name="name" value={application.name} onChange={handleChange} placeholder='Enter name' />
-            {errors.name && <span style={{color:'red', fontSize:"12px"}}>{errors.name}</span>}
+            <input type='text' name="name" value={values.name} onChange={handleChange} placeholder='Enter name' />
+            {errors.name && <span style={{ color: 'red', fontSize: "12px" }}>{errors.name}</span>}
           </div>
           <div>
             <label>Email:</label>
-            <input type='email' name="email" value={application.email} onChange={handleChange} placeholder='Enter an email id' />
-            {errors.email && <span style={{color:'red', fontSize:"12px"}}>{errors.email}</span>}
+            <input type='email' name="email" value={values.email} onChange={handleChange} placeholder='Enter an email id' />
+            {errors.email && <span style={{ color: 'red', fontSize: "12px" }}>{errors.email}</span>}
           </div>
           <div>
             <label>Phone:</label>
-            <input type='number' name="phone" value={application.phone} onChange={handleChange} placeholder='Enter a phone number' />
-            {errors.phone && <span style={{color:'red', fontSize:"12px"}}>{errors.phone}</span>}
+            <input type='number' name="phone" value={values.phone} onChange={handleChange} placeholder='Enter a phone number' />
+            {errors.phone && <span style={{ color: 'red', fontSize: "12px" }}>{errors.phone}</span>}
           </div>
 
           <div className='actions'>
@@ -158,11 +151,11 @@ const ApplicationForm = ({ jobs }) => {
         <form className='form'>
           <div>
             <label>Years of experience:</label>
-            <input type='number' name="experience" value={application.experience} onChange={handleChange} placeholder='Enter name' min={0}/>
+            <input type='number' name="experience" value={values.experience} onChange={handleChange} placeholder='Enter name' min={0} />
           </div>
           <div>
             <label>Skills:</label>
-            <textarea name="skills" value={application.skills} onChange={handleChange} placeholder='Enter skills seperated by comma' />
+            <textarea name="skills" value={values.skills} onChange={handleChange} placeholder='Enter skills seperated by comma' />
           </div>
 
           <div className='actions'>
@@ -179,18 +172,18 @@ const ApplicationForm = ({ jobs }) => {
           <div>
             <label>Cover Letter:</label>
             {
-              application.file ? <input type='text' name="file" value={application.file} readOnly /> :
-                <input type='file' name="file" value={application.file} onChange={handleChange} />
+              values.file ? <input type='text' name="file" value={values.file} readOnly /> :
+                <input type='file' name="file" value={values.file} onChange={handleChange} />
             }
 
           </div>
           <div>
             <label>Preferred Start Date.:</label>
-            <input type='date' name="startDate" value={application.startDate} onChange={handleChange} placeholder='Enter an email id' />
+            <input type='date' name="startDate" value={values.startDate} onChange={handleChange} min={minDate.toISOString().slice(0, 10)} />
           </div>
 
           <div className='actions'>
-            <button type='submit' style={{ backgroundColor: "green", border:'none' }} onClick={handleSubmit}>Submit</button>
+            <button type='submit' style={{ backgroundColor: "green", border: 'none' }} onClick={handleSubmit}>Submit</button>
             <button onClick={handleBack}>Back</button>
           </div>
 
